ba751ff9c7986bd70ef82c705d875cc5fc893e7f
Karoline Busse <karoline.busse@web.de>
2014-10-06 21:03:43 +0200

    tried some things, still no success.

--- code/bracelet/digital_accel_i2c/digital_accel_i2c.ino
+++ code/bracelet/digital_accel_i2c/digital_accel_i2c.ino
@@ -3,14 +3,24 @@
 void setup(){
   Wire.begin(); //as master
   Serial.begin(115200);
+  
+  Wire.beginTransmission(0x1D);
+  Wire.send(0x00);
+  Wire.endTransmission();
+  Wire.requestFrom(0x1D, 1);
+  byte b = Wire.read();
+  Serial.println(b, BIN);
 }
 
 void loop(){
+  Wire.beginTransmission(0x1D);
+  Wire.send(0x01);
+  Wire.endTransmission();
+  
   Wire.requestFrom(0x1D, 1);
-  while(Wire.available())
-  {
-    byte c = Wire.read();
-    Serial.print("received: ");
-    Serial.println(c, DEC);
-  }
+  byte c = Wire.read();
+  Serial.print("received: ");
+  Serial.println(c, DEC);
+  
+  delay(1000);
 }
