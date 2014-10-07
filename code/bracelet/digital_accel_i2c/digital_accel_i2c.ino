42575f3d0d8f02e944220bce4f5a5ee8d6cbd2f3
Karoline Busse <karoline.busse@web.de>
2014-10-07 16:22:03 +0200

    started with some helper functions for accelerometer config and control.

--- code/bracelet/digital_accel_i2c/digital_accel_i2c.ino
+++ code/bracelet/digital_accel_i2c/digital_accel_i2c.ino
@@ -1,26 +1,52 @@
 #include <Wire.h>
 
-void setup(){
-  Wire.begin(); //as master
-  Serial.begin(115200);
-  
-  Wire.beginTransmission(0x1D);
-  Wire.send(0x00);
+#define ADDR         0x1D
+#define CTRL_REG1    0x2A
+#define ACTIVE_MASK  0x01
+
+byte read_reg(byte addr){
+  Wire.beginTransmission(ADDR);
+  Wire.send(addr);
   Wire.endTransmission();
-  Wire.requestFrom(0x1D, 1);
-  byte b = Wire.read();
-  Serial.println(b, BIN);
+  Wire.requestFrom(ADDR, 1);
+  return Wire.read();
+} 
+
+void write_reg(byte addr, byte val){
+  Wire.beginTransmission(ADDR);
+  Wire.send(addr);
+  Wire.send(val);
+  Wire.endTransmission();
+}
+
+void active_mode(){
+  byte current = read_reg(CTRL_REG1);
+
+void setup(){
+  Serial.begin(115200);
+  Serial.println("Serial ready");
+  Wire.begin(); //as master
+  
+  Wire.beginTransmission(ADDR);
+  Wire.send(0x2A);
+  Wire.send(0x01); //set active
+  Wire.endTransmission();
+  //Wire.requestFrom(0x1D, 1);
+  //byte b = Wire.read();
+  //Serial.println(b, BIN);
+  //Serial.println("---");
+
 }
 
 void loop(){
-  Wire.beginTransmission(0x1D);
+  Wire.beginTransmission(ADDR);
   Wire.send(0x01);
   Wire.endTransmission();
   
   Wire.requestFrom(0x1D, 1);
   byte c = Wire.read();
-  Serial.print("received: ");
-  Serial.println(c, DEC);
+  //Serial.print("received: ");
+  Serial.println(c, BIN);
   
   delay(1000);
 }
