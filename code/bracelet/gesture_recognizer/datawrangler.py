e5c3c7a05b8221be7eddc69186406904a6fcc894
Karoline Busse <karoline.busse@web.de>
2014-11-05 22:07:50 +0100

    helper script for recording gesture data of random ppl

---
+++ code/bracelet/gesture_recognizer/datawrangler.py
@@ -0,0 +1,16 @@
+import serial
+
+s = serial.Serial('COM7', 115200)
+filename = 1
+
+while True:
+	line = s.readline().decode('ascii')
+	if line == "start recording\r\n":
+		f = open(str(filename)+".txt", "w")
+		for i in range(150): #current sample size is 150
+			gesture = []
+			line = s.readline().decode('ascii')
+			line = line.replace('\r', '')
+			f.write(line)
+		f.close()
+		filename += 1
