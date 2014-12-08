5ed2510936867e01aa533f1c01fa8cffe39935f3
Karoline Busse <kikidergecko@hannover.ccc.de>
2014-12-08 18:19:53 +0100

    some code to pick the best templates from the recorded gestures
    not sure if this works as intended with more than one template per
    gesture.

---
+++ code/bracelet/gesture_recognizer/pick_templates.py
@@ -0,0 +1,86 @@
+import onedollar
+
+# read gesture data from file
+# each line must contain x, y, z values
+# separated by spaces
+def file2gesture(fname):
+	f = open(fname, "r")
+	data = f.readlines()
+	gesture = []
+	for line in data:
+		coords = line.split(' ')
+		p = onedollar.Point(float(coords[0]), float(coords[1]), float(coords[2].strip('\n')))
+		gesture.append(p)
+	#recognizer preprocessing
+	gesture = onedollar.resample(gesture, 64) #magic number for the start
+	gesture = onedollar.rotate_to_zero(gesture)
+	gesture = onedollar.scale_to_square(gesture, 100) #magic number from 3$ paper
+	gesture = onedollar.translate_to_origin(gesture)
+	f.close()
+	return gesture
+
+#start by sorting the recorded samples
+zorro = []
+circle = []
+pigtail = []
+
+for i in range(1, 246):
+	path = "sample_data/" + str(i) + ".txt"
+	if (i <= 10) or ((i >= 31) and (i <= 40)) or ((i >= 63) and (i <= 72)) or ((i >= 93) and (i <= 102)) or ((i >=123) and (i <= 132)) or ((i >= 153) and (i <= 162)) or ((i >= 183) and (i <= 192)) or ((i >= 215) and (i <= 224)):  #zorro
+		if i != 95: #niete
+			zorro.append(file2gesture(path))
+	elif ((i >= 11) and (i <= 20)) or ((i >= 41) and (i <= 52)) or ((i >= 73) and (i <= 82)) or ((i >= 103) and (i <= 112)) or ((i >= 133) and (i <= 142)) or ((i >= 163) and (i <= 172)) or ((i >= 193) and (i <= 202)) or ((i >= 225) and (i <= 234)): #circle
+		circle.append(file2gesture(path))
+	elif (i != 95) and (i != 213) and (i != 214): #nieten ausfiltern
+		pigtail.append(file2gesture(path))
+
+#print("zorro : " + str(len(zorro)))
+#print("circle: " + str(len(circle)))
+#print("pigtail: " + str(len(pigtail)))
+
+data = [circle, pigtail, zorro]
+names = ["circle", "pigtail", "zorro"]
+
+max_correct = [0, 0, 0]
+id_correct = [0, 0, 0]
+min_fp = [200, 200, 200]
+id_fp = [0, 0, 0]
+
+templates = [circle[0], circle[66], pigtail[0], pigtail[65], zorro[0], zorro[12]]
+for k in range(len(data)):
+	cur_list = data[k]
+	for j in range(len(cur_list)):
+		correct_matches = 0
+		false_positives = 0
+		templates[2*k] = cur_list[j]
+		for i in range(len(cur_list)):
+			result = onedollar.recognize(cur_list[i], templates, 100)
+			if (templates.index(result[0]) == 2*k) or (templates.index(result[0]) == 2*k + 1):
+				correct_matches += 1
+			else:
+				false_positives += 1
+		print(names[k] + "[" + str(j) + "]: correct " + str(correct_matches) + " false positives: " + str(false_positives))
+		if correct_matches > max_correct[k]:
+			max_correct[k] = correct_matches
+			id_correct[k] = j
+			print("NEW best match: " + str(max_correct[k]))
+		if false_positives < min_fp[k]:
+			min_fp[k] = false_positives
+			id_fp[k] = j
+			print("NEW least error: " + str(min_fp[k]))
+		if false_positives == 0: #perfect match
+			break
+		print("--------------------")
+	
+	print("SUMMARY")
+	print("Best match: " + str(id_correct[k]) + " with " + str(float(max_correct[k])/len(cur_list)) + " (" + str(max_correct[k]) + "/" + str(len(cur_list)) + ") matches")
+	print("Least false positives: " + str(id_fp[k]) + " with " + str(float(min_fp[k])/(len(data[0]) + len(data[1]) + len(data[2]) - len(cur_list))) + " (" + str(min_fp[k]) + "/" + str((len(data[0]) + len(data[1]) + len(data[2]) - len(cur_list))) + ") false positives")
+	#templates[k] = cur_list[0] #reset for equal conditions
+
+print("--------------------")
+print("DONE!")
+print("Best templates are: ")
+for i in range(3):
+	print(names[i] + " template " + str(id_correct[i]) + " with " + str(max_correct[i]) + "/" + str(len(data[i])) + " correct matches")
+print()
+print(":)")
